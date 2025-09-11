e94df2ab3f6f76ce357c70bd4e0a1164d96ced5c
Anuradha Nayanajith <65272187+anuradha99n@users.noreply.github.com>
2025-09-11 23:27:34 +0530

    add_role_entity

---
+++ src/main/java/com/my_projects/Tea_Manager/entity/RoleEntity.java
@@ -0,0 +1,43 @@
+package com.my_projects.Tea_Manager.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Role")
+public class RoleEntity {
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String roleId;
+    @Column(nullable = false)
+    private String roleName;
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "created_user_id", nullable = false)
+    private EmployeeEntity createdUser;
+    private LocalDate createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_user_id")
+    private EmployeeEntity updatedUser;
+    @Column(nullable = true)
+    private LocalDate updatedDate;
+
+
+    @PrePersist
+    protected void onCreate(){
+
+        createdDate = LocalDate.now();
+        isActive = true;
+    }
+
+
+}
